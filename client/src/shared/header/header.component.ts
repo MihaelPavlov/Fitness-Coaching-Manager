5d2b7a871512dcba0d3aa5c9fcd8d1e2e9015214
Petar Cholakov <petar.cholakov30@gmail.com>
2024-05-22 15:21:56 +0100

    set up navigation for logged users

--- client/src/shared/header/header.component.ts
+++ client/src/shared/header/header.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  isAuth = true;
 }

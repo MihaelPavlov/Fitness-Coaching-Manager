76e8ad5ea22830a65990e26b6f451b61940af7eb
Miroslav Ivanov <znaka5@abv.bg>
2024-05-23 12:37:25 +0300

    AT-4 Add Login Form

--- client/src/app/app-routes/app.routes.ts
+++ client/src/app/app-routes/app.routes.ts
@@ -1,21 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../../pages/login/component/login.component';
 
 export const ROUTES: Routes = [
-//   {
-//     path: '',
-//     component: LayoutComponent,
-//     children: [
-//       {
-//         path: 'patient',
-//         loadChildren: () =>
-//           import('../../pages/patients/patients.module').then(
-//             (m) => m.PatientsModule
-//           ),
-//       },
-//     ],
-//   },
-//   {
-//     path: 'login',
-//     component: LoginComponent,
-//   },
+    /* {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'patient',
+                loadChildren: () =>
+                    import('../../pages/patients/patients.module').then(
+                        (m) => m.PatientsModule
+                    ),
+            },
+        ],
+    }, */
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
 ];

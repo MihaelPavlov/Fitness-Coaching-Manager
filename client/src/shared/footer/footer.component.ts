041f2a38755e1da098b3fecd451824bd6bbd87fd
Petar Cholakov <petar.cholakov30@gmail.com>
2024-05-25 06:18:13 +0100

    set up footer layout for both mobile and desktop

--- client/src/shared/footer/footer.component.ts
+++ client/src/shared/footer/footer.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  isAuth = true;
 }

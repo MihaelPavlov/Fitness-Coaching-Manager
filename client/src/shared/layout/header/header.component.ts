759a8620fb1d528c62ce07eb35be1de1347723d3
Petar Cholakov <petar.cholakov30@gmail.com>
2024-05-29 15:00:03 +0100

    fixed issue with header - no more double nav tag

--- client/src/shared/layout/header/header.component.ts
+++ client/src/shared/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isAuth = true;
+
+  @Input() pageName: string = 'Exercises'
 }

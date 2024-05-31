a39eadbb0c6648b82f34fea0ff47ff2b58069a81
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-31 18:48:09 +0300

    AT-9 Update createDatabase.ts

--- server/src/createDatabase.ts
+++ server/src/createDatabase.ts
@@ -1,4 +1,20 @@
-
 import * as configs from "./config/db.config";
 
+const customConnection = {
+    host: configs.DB_HOST,
+    user: configs.DB_USERNAME,
+    password: configs.DB_PASSWORD
+}
 
+import knex from "knex";
+
+const knexConnect = knex({ client: "mysql2", connection: customConnection });
+
+knexConnect.raw(`CREATE DATABASE IF NOT EXISTS ${configs.DB_NAME}`)
+    .then(() => {
+        knexConnect.destroy();
+        console.log("Successfully created database or exsisted.")
+    })
+    .catch((err) => {
+        console.log(err)
+    });

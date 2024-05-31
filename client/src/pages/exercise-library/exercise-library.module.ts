3d8194a7ef33dcc9debbfd54bf2744ae024c5b0b
Petar Cholakov <petar.cholakov30@gmail.com>
2024-05-31 15:24:54 +0100

    create exercise-library module

---
+++ client/src/pages/exercise-library/exercise-library.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExerciseLibraryComponent } from './component/exercise-library.component';
+
+@NgModule({
+  declarations: [ExerciseLibraryComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ExerciseLibraryComponent
+  ]
+})
+export class ExerciseLibraryModule { }

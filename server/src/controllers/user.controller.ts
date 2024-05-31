aede745ff73204d8008c0a5ad76b66fda4f505e3
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-31 18:57:52 +0300

    AT-9 Create User Controller

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,71 @@
+import express from "express";
+
+import * as userService from "./../services/user.service";
+
+const router = express.Router();
+
+router.get("/getList", async (req: express.Request, res: express.Response) => {
+  const users = await userService.getUsers(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      users,
+    },
+  });
+});
+
+router.get("/getDetail", async (req: express.Request, res: express.Response) => {
+  const user = await userService.getUser(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user
+    },
+  });
+});
+
+router.post("/create", async (req: express.Request, res: express.Response) => {
+  try {
+    const createdUser = await userService.createUser(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        message: "Successfully created user!",
+        user: createdUser,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      data: {
+        message: err.message,
+      },
+    });
+  }
+});
+
+router.post("/createSpecs", async (req: express.Request, res: express.Response) => {
+  try {
+    const createdUserSpecs = await userService.createUserSpecs(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        message: "Successfully created user specs!",
+        user: createdUserSpecs,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      data: {
+        message: err.message,
+      },
+    });
+  }
+});
+
+export default router;

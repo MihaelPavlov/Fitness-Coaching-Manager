61c8c2e18e36c8e77a3f31fa6d4d5ba94ed9b084
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-31 18:58:02 +0300

    AT-9 Create User Service

---
+++ server/src/services/user.service.ts
@@ -0,0 +1,34 @@
+import db from "./../database/db";
+import UserBuilder from "./../builders/user.builder";
+
+export const getUsers = async (payload: any) => {
+    let builder = new UserBuilder;
+    if (payload.limit !== undefined) {
+        builder.limit = payload.limit;
+    }
+    if (payload.offset !== undefined) {
+        builder.offset = payload.offset;
+    }
+    if (payload.what !== undefined) {
+        builder.fields = payload.what;
+    }
+    if (payload.condition !== undefined) {
+        builder.condition = payload.condition;
+    }
+
+    return builder.executeQuery();
+};
+
+export const getUser = async (payload: any) => {
+    let builder = new UserBuilder;
+    if (payload.what !== undefined) {
+        builder.fields = payload.what;
+    }
+    builder.id = payload.id;
+
+    return builder.executeQuery();
+};
+
+export const createUser = async (data: any) => db("users").insert(data);
+
+export const createUserSpecs = async (data: any) => db("user_specs").insert(data);

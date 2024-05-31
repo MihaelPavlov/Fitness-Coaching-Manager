0cf0612e71fc6ee9d0b0bec91e782957bf9b77a9
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-31 19:01:35 +0300

    AT-9 Update Database name in configs

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -5,7 +5,7 @@ export const DB_USERNAME = "root";
 
 export const DB_PASSWORD = "admin56Mn7=33Zz";
 
-export const DB_NAME = "athleans-app";
+export const DB_NAME = "athleans_app";
 
 export const DB_HOST = "localhost";
 

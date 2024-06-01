7aeffd713380e0753c7c77d9c6b83432b8295929
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-01 12:41:08 +0300

    AT-9 Update types.ts

--- server/src/builders/types/types.ts
+++ server/src/builders/types/types.ts
@@ -1,10 +1,3 @@
-export interface QueryParams {
-  limit: number | null | undefined;
-  offset: number | null | undefined;
-  what: Record<string, number> | null | undefined;
-  condition: Condition | null | undefined;
-}
-
 export interface Condition {
   type: string;
   items: Array<ConditionItem>;
@@ -14,4 +7,10 @@ export interface ConditionItem {
   field: string;
   operation: string;
   value: any;
+}
+
+export interface AssociationItem {
+  mainField: string;
+  relatedTable: string;
+  relatedField: string;
 }

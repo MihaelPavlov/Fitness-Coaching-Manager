a0c9e191ee016aae5a12c024067797139b0f2821
Petar Cholakov <petar.cholakov30@gmail.com>
2024-06-03 09:24:49 +0100

    create exercise-list component and its own module file

---
+++ client/src/features/exercise-list/component/exercise-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './exercise-list.component.html',
+  styleUrl: './exercise-list.component.scss'
+})
+export class ExerciseListComponent {
+
+}

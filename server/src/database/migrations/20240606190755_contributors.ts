1bfe900ad4f36b5fcb452b2db5738ce8c8488a00
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-06 22:13:42 +0300

    AT-14 Update contributors migration

--- server/src/database/migrations/20240606190755_contributors.ts
+++ server/src/database/migrations/20240606190755_contributors.ts
@@ -1,10 +1,18 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("contributors", (table) => {
+        table.increments("id").primary().unsigned().unique();
+        table.integer("user_id").unsigned()
+        table.foreign("user_id").references("users.id");
+        table.date("approval_date").defaultTo(null);
+        table.string("summary", 500).defaultTo(null);
+        table.decimal("rating", 2, 1).unsigned().notNullable().defaultTo(0.0);
+        table.tinyint("is_trusted").unsigned().notNullable().defaultTo(0);
+    });
 }
 
-
 export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists("contributors");
 }
 

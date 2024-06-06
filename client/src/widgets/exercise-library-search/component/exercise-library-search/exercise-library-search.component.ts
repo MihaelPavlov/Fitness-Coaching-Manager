72765bc7e2f78ad728f8670602649beb27486b72
Petar Cholakov <petar.cholakov30@gmail.com>
2024-06-06 10:09:52 +0100

    set up functionality to open/close search filter

--- client/src/widgets/exercise-library-search/component/exercise-library-search/exercise-library-search.component.ts
+++ client/src/widgets/exercise-library-search/component/exercise-library-search/exercise-library-search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-exercise-library-search',
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ExerciseLibrarySearchComponent {
 
+  @Input() toggleFilterForm: boolean = false;
+
   filterHandler() {
-    console.log('Hello World!')
+    this.toggleFilterForm = !this.toggleFilterForm;
   }
 }

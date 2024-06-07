0914b9a720a01824d4655791195e61aa22efaa20
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-07 16:48:48 +0300

    AT-14 Update user register route

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -26,9 +26,34 @@ router.get("/getDetail", async (req: express.Request, res: express.Response) => 
 });
 
 router.post("/register", async (req: express.Request, res: express.Response) => {
-  await userService.registerUser(req.body);
+  try {
+    const [accessToken, refreshToken, session] = await userService.registerUser(req.body);
 
-  res.status(200).json({ status: "success" });
+    res.cookie("accessToken", accessToken, {
+      maxAge: 2 * 60 * 100,
+      httpOnly: true
+    });
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+        maxAge: 10 * 60 * 100
+    });
+    res.status(200).json({
+        status: "success",
+        data: {
+            session
+        }
+    });
+    
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      data: {
+          error: err.message
+      }
+    })
+  }
 });
 
 router.post("/login", async (req: express.Request, res: express.Response) => {

30ba7382caac216c98ade688de4a5cc483bf3eb8
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-07 16:10:06 +0300

    AT-14 Create generateToken method

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,6 +1,18 @@
 import { QueryParams } from "../query-builders/models/builder.models";
 import { UserBuilder } from "../query-builders/user.builder";
 import db from "../database/database-connector";
+import * as jwt from "./../lib/jwt";
+import SECRET from "./../config/secret";
+
+const generateToken = async (createdUser: any) => {
+  const payload = {
+    id: createdUser.id,
+    username: createdUser.username
+  };
+
+  const token = await jwt.sign(payload, SECRET, {});
+  return token;
+}
 
 export const getUsers = async (payload: QueryParams) => {
   let builder = new UserBuilder(payload);
@@ -20,5 +32,11 @@ const createUser = async (data: any) => db("users").insert(data);
 const createUserSpecs = async (data: any) => db("user_specs").insert(data);
 
 export const registerUser = async (data: any) => {
+  const user = await db("users").select("*").where('email', data.email);
+
+  if (user) {
+    throw new Error("User already exsist!");
+  }
+
   
 }

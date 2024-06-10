49e24e16b377edaadf0f9ad7032a98af9ffebb39
Petar Cholakov <petar.cholakov30@gmail.com>
2024-06-10 13:15:43 +0100

    change exercise library component so as to have a horizontal scroll

--- client/src/pages/exercise-library/component/exercise-library.component.ts
+++ client/src/pages/exercise-library/component/exercise-library.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-exercise-library',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './exercise-library.component.scss'
 })
 export class ExerciseLibraryComponent {
-
+  @Input() pageName: string = 'Exercises'
 }

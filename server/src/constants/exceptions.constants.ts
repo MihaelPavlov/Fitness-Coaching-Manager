a2abec4b8f53583f5397df15cf55375781b0a6e9
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-11 11:51:36 +0300

    AT-14 Create constants and exceptions consts

---
+++ server/src/constants/exceptions.constants.ts
@@ -0,0 +1,8 @@
+const EXCEPTIONS = {
+    INVALID_LOGIN: "Invalid email or password!",
+    USER_ALREADY_EXIST: "User already exists",
+    UNAUTHENTICATED: "You must authenticate!",
+    UNAUTHORIZED: "You are unauthorized!"
+}
+
+export default EXCEPTIONS;

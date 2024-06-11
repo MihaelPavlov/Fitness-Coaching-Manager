78055497caf986613db55a5b6ae52d3bb106d7a9
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2024-06-11 16:14:31 +0300

    AT-5 html refactoring

--- client/src/pages/landing/component/landing.component.ts
+++ client/src/pages/landing/component/landing.component.ts
@@ -2,26 +2,17 @@ import { Component } from '@angular/core';
 import { RegistrationType } from '../../../shared/enums/registration-type.enum';
 import { Router } from '@angular/router';
 
-// export let registeringAsTrainer: boolean
-
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrl: './landing.component.scss',
 })
 export class LandingComponent {
-  public registrationType = RegistrationType
+  public registrationType = RegistrationType;
 
   constructor(private readonly router: Router) {}
 
-  navigateToRegister(registrationType: RegistrationType) {
-    this.router.navigate(['/register/' + registrationType])
+  public navigateToRegister(registrationType: RegistrationType) {
+    this.router.navigate([`register/${registrationType}`]);
   }
-
-
-  // registerAsTrainer() {
-  //   registeringAsTrainer = true
-  // }
 }
-
-
--- client/src/pages/register/component/register.component.ts
+++ client/src/pages/register/component/register.component.ts
@@ -1,68 +1,63 @@
-import { Component, } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InputType } from '../../../shared/enums/input-types.enum';
 import { optionArrays } from '../../../shared/option-arrays';
 import { ActivatedRoute } from '@angular/router';
 import { RegistrationType } from '../../../shared/enums/registration-type.enum';
-// import { registeringAsTrainer } from '../../landing/component/landing.component';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
-  public RegistrationType = RegistrationType
+export class RegisterComponent implements OnInit {
+  public RegistrationType = RegistrationType;
   public InputType = InputType;
+  public optionArrays = optionArrays;
+  public selectedRegistrationType!: RegistrationType;
+  public isRegisteringAsTrainer = false;
+  public showGeneralDetails = false;
+  public showAdditionalDetails = false;
+  public showProofOfRightsDetails = false;
+  public showDropDownMenu = false;
+  public attachDocument = false;
+  public attachLink = false;
+  public formData: any = {};
 
-  optionArrays = optionArrays;
-  public selectedRegistrationType!: RegistrationType
-
-  isRegisteringAsTrainer = false
-
-  constructor(private route: ActivatedRoute) {    
-    const { registrationType } = route.snapshot.params
-    this.selectedRegistrationType = registrationType    
+  constructor(private route: ActivatedRoute) {}
+  
+  public ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.selectedRegistrationType = params['registrationType'];
+    });
   }
 
-
-  showGeneralDetails = false;
-  showAdditionalDetails = false;
-  showProofOfRightsDetails = false;
-  showDropDownMenu = false;
-  attachDocument = false;
-  attachLink = false;
-
-  generalDetailsVisibility() {
-    this.showGeneralDetails = !this.showGeneralDetails
+  public generalDetailsVisibility(): void {
+    this.showGeneralDetails = !this.showGeneralDetails;
     this.showAdditionalDetails = false;
-    this.showProofOfRightsDetails = false
+    this.showProofOfRightsDetails = false;
   }
 
-  additionalDetailsVisibility() {
-    this.showAdditionalDetails = !this.showAdditionalDetails
-    this.showGeneralDetails = false
-    this.showProofOfRightsDetails = false
+  public additionalDetailsVisibility(): void {
+    this.showAdditionalDetails = !this.showAdditionalDetails;
+    this.showGeneralDetails = false;
+    this.showProofOfRightsDetails = false;
   }
 
-  proofOfRightsDetailsVisibility() {
-    this.showProofOfRightsDetails = !this.showProofOfRightsDetails
+  public proofOfRightsDetailsVisibility(): void {
+    this.showProofOfRightsDetails = !this.showProofOfRightsDetails;
     this.showGeneralDetails = false;
     this.showAdditionalDetails = false;
   }
 
-  toggleDropDownMenu() {
-    this.showDropDownMenu = !this.showDropDownMenu
+  public toggleDropDownMenu(): void {
+    this.showDropDownMenu = !this.showDropDownMenu;
   }
 
-
-  formData: any = {};
-
-  handleInputChange(name: string, value: string) {
+  public handleInputChange(name: string, value: string): void {
     this.formData[name] = value;
   }
 
-  submitHandler() {
+  public submitHandler(): void {
     console.log(this.formData);
   }
-
 }

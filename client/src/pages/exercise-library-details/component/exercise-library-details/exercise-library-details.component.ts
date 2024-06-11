42ec14e2004c427217b6abb7012f1269495c09ea
Petar Cholakov <petar.cholakov30@gmail.com>
2024-06-11 12:48:31 +0100

    create exercise library details component

---
+++ client/src/pages/exercise-library-details/component/exercise-library-details/exercise-library-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-library-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './exercise-library-details.component.html',
+  styleUrl: './exercise-library-details.component.scss'
+})
+export class ExerciseLibraryDetailsComponent {
+
+}

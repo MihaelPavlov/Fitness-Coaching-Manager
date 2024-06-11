99e09e072ab150334dbee770ad4d969ac76659b3
Petar Cholakov <petar.cholakov30@gmail.com>
2024-06-11 12:37:38 +0100

    wrap each exercise card in anchor tag so as to be clickable and add routerLink that will redirect to details page

--- client/src/features/exercise-list/component/exercise-list.component.ts
+++ client/src/features/exercise-list/component/exercise-list.component.ts
@@ -8,8 +8,10 @@ import { Component, Input } from '@angular/core';
 export class ExerciseListComponent {
 
   @Input() cardText: string = 'Dumbbell Chest Press'
+  // setting card id for development purposes - it will be used in routerLink for exercise details
+  @Input() exerciseId: number = 1;
 
   //setting this variable for development purposes - it represent the number of exercises in the list
   //this would be adjusted accordingly when we have a backend and start retrieving data from it
-  exerciseCount = [1, 2, 3, 4, 5, 6, 7, 8,9,1,1,1,1,1,1];
+  exerciseCount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 1, 1, 1, 1];
 }
--- client/src/features/exercise-list/exercise-list.module.ts
+++ client/src/features/exercise-list/exercise-list.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExerciseListComponent } from './component/exercise-list.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [ExerciseListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [ExerciseListComponent]
 })

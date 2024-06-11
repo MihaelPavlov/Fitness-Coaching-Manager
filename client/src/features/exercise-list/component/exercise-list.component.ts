13b243dcc1a771e8a8224f866d6e8d67bd1b87ff
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2024-06-11 09:18:19 +0300

    AT-6 fixing the side menu. And change the logic form grid to flex

--- client/src/features/exercise-list/component/exercise-list.component.ts
+++ client/src/features/exercise-list/component/exercise-list.component.ts
@@ -11,5 +11,5 @@ export class ExerciseListComponent {
 
   //setting this variable for development purposes - it represent the number of exercises in the list
   //this would be adjusted accordingly when we have a backend and start retrieving data from it
-  exerciseCount = [1, 2, 3, 4, 5, 6, 7, 8];
+  exerciseCount = [1, 2, 3, 4, 5, 6, 7, 8,9,1,1,1,1,1,1];
 }

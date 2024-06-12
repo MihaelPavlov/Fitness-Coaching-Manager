0b093976794d5791b381a104aa575feeaca5e252
Petar Cholakov <petar.cholakov30@gmail.com>
2024-06-12 16:44:09 +0100

    remove empty lines

--- client/src/features/exercise-list/exercise-list.module.ts
+++ client/src/features/exercise-list/exercise-list.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExerciseListComponent } from './component/exercise-list.component';
 
-
-
 @NgModule({
   declarations: [ExerciseListComponent],
   imports: [

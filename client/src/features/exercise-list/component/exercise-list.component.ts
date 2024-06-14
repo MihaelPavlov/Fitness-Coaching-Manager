b22078f3df847c283769d4b0ffd68c1715c0d74f
Petar Cholakov <petar.cholakov30@gmail.com>
2024-06-14 10:56:46 +0100

    set up functionality to direct user to card details page

--- client/src/features/exercise-list/component/exercise-list.component.ts
+++ client/src/features/exercise-list/component/exercise-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-exercise-list',
@@ -8,9 +9,16 @@ import { Component, Input } from '@angular/core';
 export class ExerciseListComponent {
   @Input() cardText: string = 'Dumbbell Chest Press';
 
+  constructor(public router: Router) {}
+
   //setting this variable for development purposes - it represent the number of exercises in the list
   //this would be adjusted accordingly when we have a backend and start retrieving data from it
   exerciseCount = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1,
   ];
+
+  navigateToDetails() {
+    // again, hardocoding this route for development purposes. no backend present yet, so no way to retrieve id of the specific card
+    this.router.navigate(['exercise/list/1'])
+  }
 }

970ce458a5ed49ea5444f47e74b610e57cfe4a63
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-14 18:03:55 +0300

    AT-21 Create request interceptor

---
+++ client/src/app/app.interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable, Provider } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable, catchError } from 'rxjs';
+import { Router } from '@angular/router';
+
+const apiUrl = "http://localhost:3000"
+
+@Injectable()
+class AppInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (request.url.startsWith("/api")) {
+      request = request.clone({
+        url: request.url.replace("/api", apiUrl),
+        withCredentials: true
+      });
+    }
+
+    return next.handle(request);
+  }
+}
+
+export const appInterceptorProvider: Provider = {
+  useClass: AppInterceptor,
+  multi: true,
+  provide: HTTP_INTERCEPTORS,
+};

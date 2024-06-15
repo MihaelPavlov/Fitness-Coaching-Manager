df07b7b33249284ebcd574c6af05aaa602eb90ad
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-15 12:48:37 +0300

    AT-21 Update user service

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RestApiService } from './rest-api.service';
+import { PATH } from './path.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private httpClient: HttpClient) { }
+  constructor(private readonly apiService: RestApiService) { }
 
   login(email: string | null | undefined, password: string | null | undefined) {
-    return this.httpClient.post("/api/users/login", { email, password }); // Here we will built auth logic
+    return this.apiService.post(PATH.USERS.LOGIN, { email, password }); // Here we will built auth logic
   }
 }

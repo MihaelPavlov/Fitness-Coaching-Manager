e663e3d27ca3c908c3c5b06dc2ebdc8460552d9e
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-15 12:58:24 +0300

    AT-21 Update error module

---
+++ client/src/shared/components/error/error.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { ErrorComponent } from "./component/error.component";
+
+NgModule({
+  declarations: [
+    ErrorComponent
+  ],
+  imports: [],
+  exports: [
+    ErrorComponent
+  ]
+})
+
+export class ErrorModule {}

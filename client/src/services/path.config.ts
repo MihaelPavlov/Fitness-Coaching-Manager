3cfd111997cc161cdcc680f302ab4665efa1d625
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-15 12:47:21 +0300

    AT-21 Create api paths config

---
+++ client/src/services/path.config.ts
@@ -0,0 +1,5 @@
+export const PATH = {
+  USERS: {
+    LOGIN: "users/login"
+  }
+}
--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -5,10 +5,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
-
   constructor(private httpClient: HttpClient) { }
 
-  login(email: string, password: string) {
+  login(email: string | null | undefined, password: string | null | undefined) {
     return this.httpClient.post("/api/users/login", { email, password }); // Here we will built auth logic
   }
 }

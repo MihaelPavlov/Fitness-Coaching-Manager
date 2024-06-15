afe2835e415f5fb444085ae7c510146e3b63750a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-15 23:13:07 +0300

    AT-21 Set tokens to localStorage on login

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RestApiService } from './rest-api.service';
 import { PATH } from './path.config';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,12 @@ export class UserService {
   constructor(private readonly apiService: RestApiService) { }
 
   login(email: string | null | undefined, password: string | null | undefined) {
-    return this.apiService.post(PATH.USERS.LOGIN, { email, password }); // Here we will built auth logic
+    return this.apiService.post(PATH.USERS.LOGIN, { email, password }).pipe(
+      tap((response: any) => {
+        console.log(response);
+        localStorage.setItem("accessToken", response.data.accessToken);
+        localStorage.setItem("refreshToken", response.data.refreshToken);
+      })
+    );
   }
 }

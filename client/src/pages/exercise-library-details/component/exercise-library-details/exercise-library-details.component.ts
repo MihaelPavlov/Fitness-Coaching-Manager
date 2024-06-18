4cf23bcf5529305bd58859a4bd781adec06f1ef7
Petar Cholakov <petar.cholakov30@gmail.com>
2024-06-18 15:20:02 +0100

    set up exercise details view for mobile

--- client/src/pages/exercise-library-details/component/exercise-library-details/exercise-library-details.component.ts
+++ client/src/pages/exercise-library-details/component/exercise-library-details/exercise-library-details.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-exercise-library-details',
-  standalone: true,
-  imports: [],
   templateUrl: './exercise-library-details.component.html',
+  standalone: true,
+  imports: [CommonModule],
   styleUrl: './exercise-library-details.component.scss'
 })
 export class ExerciseLibraryDetailsComponent {
 
+  //setting this for development purposes
+  exerciseInstructions = [
+    'Begin with legs shoulder width apart and place hands on the floor in a push up position under the shoulders',
+    'Kick legs back and lower stomach and thighs to touch the floor',
+    'Push from this position while simultaniously bringing kness to the chest planting feet on the ground'
+  ]
 }

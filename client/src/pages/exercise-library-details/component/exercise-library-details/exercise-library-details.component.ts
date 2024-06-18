7cfd88e5bb67e49bfca4f89e518fe54c7374ef35
Petar Cholakov <petar.cholakov30@gmail.com>
2024-06-18 15:44:28 +0100

    set up exercise view for desktop

--- client/src/pages/exercise-library-details/component/exercise-library-details/exercise-library-details.component.ts
+++ client/src/pages/exercise-library-details/component/exercise-library-details/exercise-library-details.component.ts
@@ -14,6 +14,8 @@ export class ExerciseLibraryDetailsComponent {
   exerciseInstructions = [
     'Begin with legs shoulder width apart and place hands on the floor in a push up position under the shoulders',
     'Kick legs back and lower stomach and thighs to touch the floor',
-    'Push from this position while simultaniously bringing kness to the chest planting feet on the ground'
+    'Push from this position while simultaniously bringing kness to the chest planting feet on the ground',
+    'Extend upward and jump from this position, extending arms upwards returning to beginning position',
+    'Repeat for reps'
   ]
 }

51b872040892202b423e7b5ac409fb4cb91fb3a5
Miroslav Ivanov <znaka5@abv.bg>
2024-06-22 12:21:36 +0300

    AT-17-BE Add Exercise

--- server/src/controllers/exercises.controller.ts
+++ server/src/controllers/exercises.controller.ts
@@ -5,11 +5,27 @@ import { isCoach } from "./../middlewares/auth.middleware";
 const router = express.Router();
 
 router.post("/add", isCoach, async (req: any, res: express.Response) => {
+  try {
+    const exerciseData = req.body;
 
+    const createdExerciseID = await exerciseService.addExercise(exerciseData);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        exerciseID: createdExerciseID,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 });
 
-
 router.post("/getList", async (req: any, res: express.Response) => {
+  try {
     const exercises = await exerciseService.getExercises(req.body);
 
     res.status(200).json({
@@ -18,6 +34,12 @@ router.post("/getList", async (req: any, res: express.Response) => {
         exercises,
       },
     });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 });
 
 export default router;
--- server/src/services/exercise.service.ts
+++ server/src/services/exercise.service.ts
@@ -9,5 +9,65 @@ export const getExercises = async (payload: QueryParams) => {
 };
 
 export const addExercise = async (exerciseData: any) => {
-    
+  const requiredFields = [
+    "title",
+    "thumb_uri",
+    "difficulty",
+    "description",
+    "equipment_ids",
+    "tag_ids",
+  ];
+
+  for (let field of requiredFields) {
+    if (!exerciseData[field]) {
+      throw new Error(`Missing required field: ${field}`);
+    }
+  }
+
+  const equipmentIds = exerciseData.equipment_ids
+    ? exerciseData.equipment_ids.split(",")
+    : [];
+  const tagIds = exerciseData.tag_ids ? exerciseData.tag_ids.split(",") : [];
+
+  if (equipmentIds.length > 0) {
+    const equipmentCount = await db("exercise_equipment")
+      .whereIn("id", equipmentIds)
+      .count("* as count")
+      .first();
+    if (equipmentCount.count !== equipmentIds.length) {
+      throw new Error("Invalid equipment IDs");
+    }
+  } else {
+    throw new Error("equipment_ids must be selected");
+  }
+
+  if (tagIds.length > 0) {
+    const tagCount = await db("exercise_tags")
+      .whereIn("id", tagIds)
+      .count("* as count")
+      .first();
+    if (tagCount.count !== tagIds.length) {
+      throw new Error("Invalid tag IDs");
+    }
+  } else {
+    throw new Error("tag_ids must be selected");
+  }
+
+  const currentDate = new Date().toISOString().split("T")[0];
+
+  const createdExerciseID = (
+    await db("exercises").insert({
+      contributor_id: exerciseData.contributor_id, 
+      title: exerciseData.title,
+      thumb_uri: exerciseData.thumb_uri,
+      difficulty: exerciseData.difficulty,
+      equipment_ids: exerciseData.equipment_ids,
+      description: exerciseData.description,
+      tag_ids: exerciseData.tag_ids,
+      date_created: currentDate,
+      date_modified: currentDate,
+    })
+  ).at(0);
+
+  return createdExerciseID;
 };

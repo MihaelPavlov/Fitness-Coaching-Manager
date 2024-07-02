b4d602b07f83a0de07800d618257bf148c3233b1
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-02 15:05:30 +0300

    AT-23 Create BE validators for creating user and coach

--- server/src/validators/user.validator.ts
+++ server/src/validators/user.validator.ts
@@ -1,6 +1,8 @@
 import { EXCEPTION } from "./../constants/exceptions.constants";
 import { ValidationChain, checkSchema } from "express-validator";
 import { formatString } from "./../helpers/string-format.helper";
+import { GenderType } from "./../models/enums/gender-list.enum";
+import { FitnessLevels } from "./../models/enums/fitness-levels.enum";
 
 export const createUserValidators: ValidationChain[] = checkSchema({
     username: {
@@ -37,6 +39,96 @@ export const createUserValidators: ValidationChain[] = checkSchema({
         in: ["body"],
         exists: {
             errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "sex")
+        },
+        isIn: {
+            options: Object.values(GenderType),
+            errorMessage: formatString(EXCEPTION.VALIDATION.INVALID_VALUE, "sex")
+        }
+    },
+    fitnessLevel: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "fitnessLevel")
+        },
+        isIn: {
+            options: Object.values(FitnessLevels),
+            errorMessage: formatString(EXCEPTION.VALIDATION.INVALID_VALUE, "fitnessLevel")
+        }
+    }
+});
+
+export const createCoachValidators = checkSchema({
+    userRole: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "userRole")
+        },
+        toInt: true,
+        isInt: {
+            options: {
+                min: 1,
+                max: 1
+            },
+            errorMessage: formatString(EXCEPTION.VALIDATION.INVALID_VALUE, "userRole")
+        }
+    },
+    username: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "username")
+        }
+    },
+    email: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "email")
+        },
+        isEmail: true
+    },
+    password: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "password")
+        }
+    },
+    country: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "country")
+        }
+    },
+    language: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "language")
+        }
+    },
+    sex: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "sex")
+        },
+        isIn: {
+            options: Object.values(GenderType),
+            errorMessage: formatString(EXCEPTION.VALIDATION.INVALID_VALUE, "sex")
+        }
+    },
+    phoneNumber: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "phoneNumber")
+        }
+    },
+    firstName: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "firstName")
+        }
+    },
+    lastName: {
+        in: ["body"],
+        exists: {
+            errorMessage: formatString(EXCEPTION.VALIDATION.REQUIRED_FIELD, "lastName")
         }
     },
 });

6917aebdb5597a4ee4e978a87f0d53709338050c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-02 12:39:38 +0300

    AT-23 Update header.component.ts

--- client/src/features/layout/header/header.component.ts
+++ client/src/features/layout/header/header.component.ts
@@ -9,14 +9,14 @@ import { Observable } from 'rxjs';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
+  @Input() pageName: string = 'Header';
+
   public userInfo$: Observable<any> = this.userService.userInfo$;
   public isAuth$ = this.userService.isAuth$;
 
   constructor(private userService: UserService) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.userService.fetchUserInfo();
   }
-
-  @Input() pageName: string = 'Header';
 }

76f14c82da78c1d8ff0acae4e79369e0bf20ce13
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-05 19:37:38 +0300

    AT-16 Create user_workout_collection migration

--- server/src/database/migrations/20240705154824_user_workout_collection.ts
+++ server/src/database/migrations/20240705154824_user_workout_collection.ts
@@ -1,10 +1,16 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTableIfNotExists('user_workout_collection', (table) => {
+        table.increments('id').primary().unsigned().notNullable().unique();
+        table.integer('user_id').unsigned().notNullable();
+        table.foreign('user_id').references('users.id')
+        table.integer('workout_session_id').unsigned().notNullable();
+        table.foreign('workout_session_id').references('workout_session.id');
+    })
 }
 
-
 export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists('user_workout_collection');
 }
 

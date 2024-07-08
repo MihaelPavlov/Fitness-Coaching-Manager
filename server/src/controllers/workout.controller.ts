57e911072c554be5f37ccffed489d398f01b4876
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-08 21:47:40 +0300

    AT-16 Create workout.validator.ts

--- server/src/controllers/workout.controller.ts
+++ server/src/controllers/workout.controller.ts
@@ -1,5 +1,16 @@
+import { isAuth, isCoach } from "./../middlewares/auth.middleware";
+import { PATH } from "./../constants/path.constants";
 import express from "express";
 
 const router = express.Router();
+
+router.post(
+    PATH.WORKOUTS.CREATE_WORKOUT,
+    isAuth,
+    isCoach,
+    async (req: any, res: express.Response) => {
+        
+    }
+)
 
 export default router;

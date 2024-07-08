16121296abf73d2efab4193b979085c579716197
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-08 22:06:15 +0300

    AT-16 Finish create workout route in controller

--- server/src/controllers/workout.controller.ts
+++ server/src/controllers/workout.controller.ts
@@ -1,7 +1,9 @@
+import express from "express";
+import { createWorkoutSession } from "./../services/workout.service";
 import { isAuth, isCoach } from "./../middlewares/auth.middleware";
 import { PATH } from "./../constants/path.constants";
-import express from "express";
 import { createWorkoutValidators } from "./../validators/workout.validator";
+import { RESPONSE_STATUS } from "./../constants/response.constants";
 
 const router = express.Router();
 
@@ -11,7 +13,23 @@ router.post(
     isCoach,
     createWorkoutValidators,
     async (req: any, res: express.Response) => {
+        try {
+            const createdWorkout = await createWorkoutSession(req.user.id, req.body);
 
+            res.status(201).json({
+                status: RESPONSE_STATUS.SUCCESS,
+                data: {
+                    workout: createdWorkout
+                }
+            })
+        } catch (err) {
+            res.status(400).json({
+                status: RESPONSE_STATUS.FAILED,
+                data: {
+                    error: err.message
+                }
+            });
+        }
     }
 )
 

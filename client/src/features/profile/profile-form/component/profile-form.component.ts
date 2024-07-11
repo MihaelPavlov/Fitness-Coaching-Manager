b312bd404435e0e9b6f3b4b17bd159160d1125aa
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-11 18:13:01 +0300

    AT-15.1 Create updateUserForm builder

--- client/src/features/profile/profile-form/component/profile-form.component.ts
+++ client/src/features/profile/profile-form/component/profile-form.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { InputType } from '../../../../shared/enums/input-types.enum';
+import { IUserDetails } from '../../../../entities/users/models/user-details.interface';
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-profile-form',
   templateUrl: './profile-form.component.html',
   styleUrls: ['./profile-form.component.scss']
 })
 export class ProfileFormComponent {
+  @Input() user: IUserDetails | undefined;
+
+  constructor(private readonly fb: FormBuilder) {}
+
   public InputType = InputType;
+
+  public updateUserForm = this.fb.group({
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    birthDate: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    weightGoal: ['', [Validators.required]],
+    weight: ['', [Validators.required]]
+  });
 }

fba5ad82d3db6339606ece3734343ff75eb010d1
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-11 18:26:50 +0300

    AT-15.1 Dynamically fulfill update user form

--- client/src/features/profile/profile-form/component/profile-form.component.ts
+++ client/src/features/profile/profile-form/component/profile-form.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { InputType } from '../../../../shared/enums/input-types.enum';
 import { IUserDetails } from '../../../../entities/users/models/user-details.interface';
 import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../../../../entities/users/services/user.service';
 @Component({
   selector: 'app-profile-form',
   templateUrl: './profile-form.component.html',
   styleUrls: ['./profile-form.component.scss']
 })
-export class ProfileFormComponent {
+export class ProfileFormComponent implements OnChanges {
   @Input() user: IUserDetails | undefined;
 
-  constructor(private readonly fb: FormBuilder) {}
+  constructor(private readonly fb: FormBuilder, private readonly userService: UserService) {}
 
   public InputType = InputType;
 
@@ -22,4 +23,18 @@ export class ProfileFormComponent {
     weightGoal: ['', [Validators.required]],
     weight: ['', [Validators.required]]
   });
+
+  ngOnChanges(changes: SimpleChanges): void {
+      if (changes['user']) {
+        for (let [field, value] of Object.entries(this.user as object)) {
+          this.updateUserForm.get(field)?.setValue(value);
+        }
+      }
+  }
+
+  public onUpdate(): void {
+    if (this.updateUserForm.invalid) {
+      console.log("Invalid form");
+    }
+  }
 }

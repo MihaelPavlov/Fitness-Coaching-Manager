b1c0737a92fad12c92eb55d4e046888d38a9fb30
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-18 22:55:21 +0300

    AT-29 Set 10MB uploaded file size limit

--- server/src/config/file-upload.config.ts
+++ server/src/config/file-upload.config.ts
@@ -9,6 +9,6 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage, limits: { fileSize: 10000000 } });
 
 export default upload;

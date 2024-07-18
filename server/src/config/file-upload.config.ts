44456d04bed90b84717fdf809b386aaee058482b
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-19 01:35:52 +0300

    AT-29 Save files to local disk

--- server/src/config/file-upload.config.ts
+++ server/src/config/file-upload.config.ts
@@ -1,8 +1,11 @@
 import multer from "multer";
+import * as fs from "fs";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // specify the directory where files will be saved
+    const uploadPath = "C:/uploads/AthleansApp/Documents"
+    if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath)
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);

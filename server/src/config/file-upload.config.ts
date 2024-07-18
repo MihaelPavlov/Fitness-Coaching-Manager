5c17d5f2e6af867ddae15de8c8ac3ffe2e37f0f4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-19 00:52:26 +0300

    AT-29 Format code

--- server/src/config/file-upload.config.ts
+++ server/src/config/file-upload.config.ts
@@ -1,13 +1,13 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/'); // specify the directory where files will be saved
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname);
-    },
-  });
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // specify the directory where files will be saved
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
 const upload = multer({ storage: storage, limits: { fileSize: 10000000 } });
 

b530e009fc1015753a99ed6164647653e3ea4516
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-19 21:37:17 +0300

    AT-29 Create middleware for fetching uploaded files from server to FE

--- server/src/config/file-upload.config.ts
+++ server/src/config/file-upload.config.ts
@@ -1,11 +1,8 @@
 import multer from "multer";
-import * as fs from "fs";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const uploadPath = "C:/uploads/AthleansApp/Documents"
-    if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath)
-    cb(null, uploadPath);
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import expressConfig from "./config/express.config";
+import * as fs from "fs";
+import * as stream from "stream"
 import {
   checkAccessToken,
   checkRefreshToken,
@@ -16,6 +18,22 @@ app.use(
     credentials: true,
   })
 );
+
+// Fetch saved files from server to client
+app.get("/files/:filename", (req: express.Request, res: express.Response) => {
+  const r = fs.createReadStream(`uploads/${req.params.filename}`) // or any other way to get a readable stream
+  const ps = new stream.PassThrough() // <---- this makes a trick with stream error handling
+  stream.pipeline(
+   r,
+   ps, // <---- this makes a trick with stream error handling
+   (err) => {
+    if (err) {
+      console.log(err) // No such file or any other kind of error
+      return res.sendStatus(400); 
+    }
+  })
+  ps.pipe(res) // <---- this makes a trick with stream error handling
+});
 
 expressConfig(app);
 

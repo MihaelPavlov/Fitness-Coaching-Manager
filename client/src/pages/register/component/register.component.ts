10cf12f984cc6c6dcd185f5d32657d6ce74a37d4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 15:26:14 +0300

    AT-29 Refactor toFormData function to shared/utils

--- client/src/pages/register/component/register.component.ts
+++ client/src/pages/register/component/register.component.ts
@@ -15,6 +15,7 @@ import { UserRoles } from '../../../shared/enums/user-roles.enum';
 import { GenderType } from '../../../shared/enums/gender-list.enum';
 import { FitnessLevels } from '../../../shared/enums/fitness-levels.enum';
 import { AuthService } from '../../../entities/users/services/auth.service';
+import { toFormData } from '../../../shared/utils/formTransformer';
 
 @Component({
   selector: 'app-register',
@@ -156,7 +157,7 @@ export class RegisterComponent implements OnInit {
     delete requestBody['passGroup'];
     console.log(requestBody);
 
-    this.authService.register(this.toFormData(requestBody)).subscribe({
+    this.authService.register(toFormData(requestBody)).subscribe({
       next: () => {
         this.isLoading = false;
         this.hasRegisterError = false;
@@ -210,21 +211,5 @@ export class RegisterComponent implements OnInit {
         ? null
         : { matchPasswordsValidator: true };
     };
-  }
-
-  private toFormData(form: any) {
-    const formData = new FormData();
-
-    for (const [key, value] of Object.entries(form)) {
-      if (key === 'files') {
-        for (let file of form['files']) {
-          formData.append(key, file);
-        }
-        continue;
-      }
-      formData.append(key, value as File | string);
-    }
-
-    return formData;
   }
 }

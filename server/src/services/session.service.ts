d821ddadd9343d2c624e3edde4ce95eb5a93fd1f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-25 22:21:07 +0300

    AT-36 Create session service BE

---
+++ server/src/services/session.service.ts
@@ -0,0 +1,7 @@
+import { SessionExercisesBuilder } from "./../query-builders/session-exercises.builder"
+import { QueryParams } from "./../query-builders/models/builder.models"
+
+export const getSessionExercises = async (queryParams: QueryParams) => {
+    const builder = new SessionExercisesBuilder(queryParams);
+    const results = await builder.buildQuery();
+}

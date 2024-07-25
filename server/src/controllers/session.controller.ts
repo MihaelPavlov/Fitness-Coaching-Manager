c4a521904fdfec5c56ff67927a14cfa6c6643e67
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-25 22:25:54 +0300

    AT-36 Implement get session exercises route

--- server/src/controllers/session.controller.ts
+++ server/src/controllers/session.controller.ts
@@ -1,5 +1,24 @@
-import express from "express";
+import express, { NextFunction } from "express";
+
+import * as sessionService from "./../services/session.service";
+import { RESPONSE_STATUS } from "./../constants/response.constants";
 
 const router = express.Router();
+
+router.post(
+    "/getExercises",
+    async (req: express.Request, res: express.Response, next: NextFunction) => {
+        try {
+            const exercises = await sessionService.getSessionExercises(req.body);
+
+            res.status(200).json({
+                status: RESPONSE_STATUS.SUCCESS,
+                data: exercises
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
+)
 
 export default router;

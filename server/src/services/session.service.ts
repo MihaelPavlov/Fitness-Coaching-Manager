e2304c472788c30b0f6cc5f201e17cc06dd30787
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-25 22:42:16 +0300

    AT-36 Map session exercises return result

--- server/src/services/session.service.ts
+++ server/src/services/session.service.ts
@@ -1,9 +1,10 @@
 import { SessionExercisesBuilder } from "./../query-builders/session-exercises.builder"
 import { QueryParams } from "./../query-builders/models/builder.models"
+import { mapSessionExercises } from "./../helpers/session.helper";
 
 export const getSessionExercises = async (queryParams: QueryParams) => {
     const builder = new SessionExercisesBuilder(queryParams);
     const results = await builder.buildQuery();
-
-    return results;
+    
+    return await mapSessionExercises(results);
 }

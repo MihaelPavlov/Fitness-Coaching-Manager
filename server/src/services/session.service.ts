bb7f81ed2f89f4bcdfa08e7e737ab0f493f0af56
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-25 22:23:23 +0300

    AT-36 Create session controller

--- server/src/services/session.service.ts
+++ server/src/services/session.service.ts
@@ -4,4 +4,6 @@ import { QueryParams } from "./../query-builders/models/builder.models"
 export const getSessionExercises = async (queryParams: QueryParams) => {
     const builder = new SessionExercisesBuilder(queryParams);
     const results = await builder.buildQuery();
+
+    return results;
 }

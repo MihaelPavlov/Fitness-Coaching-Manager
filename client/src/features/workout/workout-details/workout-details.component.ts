1f2e92e9ca8e116b6ff2c540f0bf40bb968d6af0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-31 15:27:31 +0300

    AT-28 Get workout id from route

--- client/src/features/workout/workout-details/workout-details.component.ts
+++ client/src/features/workout/workout-details/workout-details.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { WorkoutService } from '../../../entities/workouts/services/workout.service';
 
 @Component({
   selector: 'app-workout-details',
   templateUrl: './workout-details.component.html',
   styleUrl: './workout-details.component.scss',
 })
-export class WorkoutDetailsComponent {
-  workoutName:string = 'Chest Workout'
+export class WorkoutDetailsComponent implements OnInit {
+  public workoutId?: number;
+  public workoutName:string = 'Chest Workout';
+  public exercises?: any;
 
-  constructor(private readonly router:Router){}
+  constructor(
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
+    private readonly workoutService: WorkoutService
+  ){}
 
-  navigateToDetails(){
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.workoutId = params['workoutId'];
+    })
+  }
+
+  public navigateToDetails(){
     this.router.navigateByUrl("exercise/details/1");
+  }
+
+  public navigateToSession() {
+    this.router.navigate([`/workouts/session/${this.workoutId}`]);
+  }
+
+  private fetchWorkout(workoutId: number) {
+
   }
 }

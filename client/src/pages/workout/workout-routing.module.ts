17e363ec925257449318c54ba03526f9d74dfa28
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-31 15:19:17 +0300

    AT-28 Update workout routing

--- client/src/pages/workout/workout-routing.module.ts
+++ client/src/pages/workout/workout-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: WorkoutLibraryComponent,
   },
   {
-    path: 'details',
+    path: 'details/:workoutId',
     component: WorkoutDetailsComponent,
   },
   {

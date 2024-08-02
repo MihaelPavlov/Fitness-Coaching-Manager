210f59bef9f3e95995538900c73bdac424718511
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-02 13:35:24 +0300

    AT-18 Navigate to contributor on click

--- client/src/features/contributor/contributor-card-new/contributor-card-new.component.ts
+++ client/src/features/contributor/contributor-card-new/contributor-card-new.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { IContributor } from '../../../entities/contributors/models/contributors.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contributor-card-new',
@@ -7,5 +8,13 @@ import { IContributor } from '../../../entities/contributors/models/contributors
   styleUrl: './contributor-card-new.component.scss',
 })
 export class ContributorCardNewComponent {
-  @Input() contributor?: IContributor
+  @Input() contributor?: IContributor;
+
+  constructor(
+    private readonly router: Router
+  ) {}
+
+  public navigateToContributor() {
+    this.router.navigate([`/profile/${this.contributor?.userId}`]);
+  }
 }

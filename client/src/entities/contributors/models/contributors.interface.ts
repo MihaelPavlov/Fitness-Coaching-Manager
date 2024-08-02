35af76690d9fee269d05a202e6967b313bbda00b
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-02 12:15:05 +0300

    Create Contributors interface

---
+++ client/src/entities/contributors/models/contributors.interface.ts
@@ -0,0 +1,10 @@
+export interface Contributors {
+  contributorId: number;
+  userId: number;
+  profilePicture: string;
+  firstName: string;
+  lastName: string;
+  rating: number;
+  dateCreated: any;
+  subscribersCount: number
+}

7c6ad58bf5005ba585f88a2fb54f900f27658ff1
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2024-08-03 00:18:17 +0300

    at-18 fix bug with subscribers

--- server/src/controllers/contributor.controller.ts
+++ server/src/controllers/contributor.controller.ts
@@ -28,7 +28,7 @@ router.post(
     isCoach,
     async (req: any, res: express.Response, next: express.NextFunction) => {
         try {
-            const subscribers = await contributorService.getContributorSubscribers(req.user.id, req.body);
+            const subscribers = await contributorService.getContributorSubscribers(req.user.contributorId, req.body);
 
             res.status(200).json({
                 status: RESPONSE_STATUS.SUCCESS,

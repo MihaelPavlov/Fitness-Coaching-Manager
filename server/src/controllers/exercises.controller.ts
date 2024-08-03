b24d5c0bf65d4eb59bd4dd354c652ceefa56cd76
Miroslav Ivanov <znaka5@abv.bg>
2024-08-03 21:59:22 +0300

    AT-38 BE Exercise

--- server/src/controllers/exercises.controller.ts
+++ server/src/controllers/exercises.controller.ts
@@ -46,28 +46,31 @@ router.put(
 );
 
 router.delete(
-  `${PATH.EXERCISES.DELETE}/:exerciseId`,
-  isAuth,
-  isCoach,
-  async (req: any, res: express.Response) => {
-    try {
-      await exerciseService.deleteExercise(Number(req.params.exerciseId), req.user.id);
+    PATH.EXERCISES.DELETE,
+    isAuth,
+    isCoach,
+    async (req: any, res: express.Response) => {
+        try {
+            await exerciseService.deleteExercise(
+                Number(req.params.exerciseId),
+                req.user.id
+            );
 
-      res.status(200).json({
-        status: RESPONSE_STATUS.SUCCESS,
-        data: {
-          message: "Successfully deleted exercise!"
+            res.status(200).json({
+                status: RESPONSE_STATUS.SUCCESS,
+                data: {
+                    message: "Successfully deleted exercise!",
+                },
+            });
+        } catch (err) {
+            return res.status(400).json({
+                status: RESPONSE_STATUS.FAILED,
+                data: {
+                    error: err.message,
+                },
+            });
         }
-      });
-    } catch (err) {
-      return res.status(400).json({
-        status: RESPONSE_STATUS.FAILED,
-        data: {
-          error: err.message,
-        },
-      });
     }
-  }
 );
 
 router.post(

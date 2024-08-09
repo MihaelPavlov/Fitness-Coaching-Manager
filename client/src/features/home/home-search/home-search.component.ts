b69bf6208e724761b86ebdabf156c51708bc0c12
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-09 21:45:11 +0300

    Bug-13 Fix home page filtering and searching

--- client/src/features/home/home-search/home-search.component.ts
+++ client/src/features/home/home-search/home-search.component.ts
@@ -2,6 +2,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { IWorkoutTag } from '../../../entities/workouts/models/workout-tag.interface';
 import { IWorkout } from '../../../entities/workouts/models/workout.interface';
+import { UserService } from '../../../entities/users/services/user.service';
+import { WorkoutService } from '../../../entities/workouts/services/workout.service';
+import { IQueryParams } from '../../../entities/models/query-params.interface';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home-search',
@@ -14,7 +18,7 @@ export class HomeSearchComponent implements OnInit {
   @Input() allWorkouts: IWorkout[] = [];
   @Input() isLoadingSubject?: BehaviorSubject<boolean>;
   @Input() tags?: IWorkoutTag[];
-  @Input() selectedTagsSubject?: BehaviorSubject<IWorkoutTag[]>;
+  public selectedTagsSubject = new BehaviorSubject<IWorkoutTag[]>([]);
 
   public selectedTags?: IWorkoutTag[];
   public searchValue: string = '';
@@ -24,6 +28,11 @@ export class HomeSearchComponent implements OnInit {
       this.selectedTags = values;
     });
   }
+
+  constructor(
+    private readonly userService: UserService,
+    private readonly workoutService: WorkoutService
+  ) {}
 
   public onSearchChange(event: Event) {
     const value = (event.target as HTMLInputElement).value;
@@ -41,9 +50,10 @@ export class HomeSearchComponent implements OnInit {
       );
     }
     this.selectedTagsSubject?.next(this.selectedTags || []);
+    this.search();
   }
 
-  public search(): void {
+  /* public search(): void {
     const filtered = this.allWorkouts.filter((workout) => {
       return workout.title
         .toLowerCase()
@@ -51,6 +61,102 @@ export class HomeSearchComponent implements OnInit {
     });
 
     this.workoutsSubject?.next(filtered || []);
+  } */
+
+  public search(): void {
+    this.isLoadingSubject?.next(true);
+    let queryParams: IQueryParams | null = null;
+    if (this.userService.getUser?.contributorId) {
+      queryParams = {
+        what: {
+          uid: 1,
+          title: 1,
+          owner: 1,
+          tags: 1,
+          rating: 1,
+          imageUri: 1,
+        },
+        condition: {
+          type: 'AND',
+          items: [
+            {
+              field: 'owner',
+              operation: 'EQ',
+              value: this.userService.getUser?.contributorId,
+            },
+          ],
+        },
+      };
+      this.fetchWorkouts(queryParams);
+    } else {
+      this.userService
+        .getUserWorkoutList({
+          what: {
+            userId: 1,
+            workoutSessionId: 1,
+          },
+          condition: {
+            type: 'AND',
+            items: [
+              {
+                field: 'userId',
+                operation: 'EQ',
+                value: this.userService.getUser?.id,
+              },
+            ],
+          },
+        })
+        .subscribe({
+          next: (x) => {
+            if (x?.data) {
+              const coniditionItems = x.data.map((x) => ({
+                field: 'uid',
+                operation: 'EQ',
+                value: x.workoutSessionId,
+              }));
+              if (coniditionItems.length !== 0) {
+                queryParams = {
+                  what: {
+                    uid: 1,
+                    title: 1,
+                    owner: 1,
+                    tags: 1,
+                    rating: 1,
+                    imageUri: 1,
+                  },
+                  condition: {
+                    type: 'OR',
+                    items: coniditionItems,
+                  },
+                };
+                this.fetchWorkouts(queryParams);
+              } else {
+                this.allWorkouts = [];
+                this.workoutsSubject?.next([]);
+              }
+            }
+            this.isLoadingSubject?.next(true);
+          },
+          error: (err) => console.log(err),
+          complete: () => this.isLoadingSubject?.next(false),
+        });
+    }
+  }
+
+  private fetchWorkouts(queryParams: IQueryParams) {
+    this.workoutService
+      .getWorkouts(queryParams, this.searchValue, this.mapSelectedTags())
+      .subscribe({
+        next: (res) => {
+          this.workoutsSubject?.next(res?.data || []);
+        },
+        error: (err) => console.log(err),
+        complete: () => this.isLoadingSubject?.next(false),
+      });
+  }
+
+  private mapSelectedTags() {
+    return this.selectedTags?.map((el: any) => el.uid).join(",") || null;
   }
 
   public filterHandler(): void {
